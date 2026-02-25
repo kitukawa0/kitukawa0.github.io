c8c48271a10316e3be9e63b745f4de5481a06c8d
Merkulov Daniil <kitukawa0@gmail.com>
2026-02-25 17:43:36 +0200

    resolved conflicts

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 function copyIP() {
     const ip = document.getElementById("server-ip").innerText;
     navigator.clipboard.writeText(ip);
     alert("IP скопирован!");
-=======
-function copyIP() {
-    const ip = document.getElementById("server-ip").innerText;
-    navigator.clipboard.writeText(ip);
-    alert("IP скопирован!");
->>>>>>> 5140420246feaf995a40ccae303483a272d522ef
 }
